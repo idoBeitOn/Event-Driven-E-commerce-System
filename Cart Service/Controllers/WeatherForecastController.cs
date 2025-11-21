ddbd7a6d58d850f607eea5df9d04951d654d07ab
idoBeitOn <beiton.ido@gmail.com>
2025-11-21 13:26:53 +0200

    Producer(Cart Service) - Added OrderRequset+Order

---
+++ Cart Service/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cart_Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+    }
+}
