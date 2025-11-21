e17faef694cbcbc4ee1da8245074f2afe259ce67
idoBeitOn <beiton.ido@gmail.com>
2025-11-21 21:08:07 +0200

    Create TotalsDTO.cs

---
+++ Cart Service/Models/Orders/TotalsDTO.cs
@@ -0,0 +1,11 @@
+namespace Cart_Service.Models.Orders;
+
+public sealed class TotalsDto
+{
+    public decimal SubTotal { get; init; }
+    public decimal Tax { get; init; }
+    public decimal Discount { get; init; }
+    public decimal TotalAmount { get; init; }
+    public string Currency { get; init; } = "USD";
+}
+
