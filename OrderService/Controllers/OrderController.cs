6efd9895d471e0fa9bb9e37a7c52fc98ef9f81fc
idoBeitOn <beiton.ido@gmail.com>
2025-11-22 20:10:10 +0200

    Added IOrderConsumer - Interface for consuming Order messages

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedDTOs;
+namespace OrderService.Controllers
+
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+    }
+}
---
+++ OrderService/Logic/Interfaces/IOrderConsumer.cs
@@ -0,0 +1,12 @@
+﻿using SharedDTOs;
+
+namespace OrderService.Logic.Interfaces
+{
+
+    //Abstracts the RabbitMQ consuming logic.
+    //De-serialize JSON to OrderDTO type variable in order to make calculations.
+    public interface IOrderConsumer
+    {
+        Task ConsumeOrderAsync(OrderDTO orderDTO, CancellationToken cancellationToken = default);
+    }
+}
